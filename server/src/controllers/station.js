c618bc037e38d938510a677883b7f54eaabb931e
Walber Rocha <walber_jesus@hotmail.com>
2022-03-09 01:11:13 -0300

    fix(server) Remoção de log

--- server/src/controllers/station.js
+++ server/src/controllers/station.js
@@ -1,6 +1,5 @@
 module.exports = {
     updateValues(request, response) {
-        console.log(request.io)
         request.io.emit('updateValues', request.body);
         return response.json(request.body);
     }
